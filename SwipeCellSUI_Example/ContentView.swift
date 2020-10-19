dd88dc43a77f6c2c19f37035c57c421e59b94cdf
Dominik <dominikbutz@gmail.com>
2020-10-19 13:59:15 +0800

    Initial Commit

---
+++ SwipeCellSUI_Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwipeCellSUI_Example
+//
+//  Created by Dominik Butz on 19/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwipeCellSUI_Example/SwipeCellSUI_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeCellSUI_ExampleApp.swift
+//  SwipeCellSUI_Example
+//
+//  Created by Dominik Butz on 19/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwipeCellSUI_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
